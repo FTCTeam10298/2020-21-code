5c416088d4932fab5a2aa3c3aa930e799681b9df
james <sj2@penrose.us>
2020-10-20 20:19:05 -0700

    enabled proper drivetrain movement without lBDrive motor encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimBotMovement.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimBotMovement.kt
@@ -13,6 +13,10 @@ open class AimBotMovement(): AimBotHardware() {
         lBDrive?.power = lBpower
     }
 
+    fun stopMotors() {
+        fourMotors(0.0,0.0,0.0,0.0,)
+    }
+
     fun moveTime(rFpower: Double, lFpower: Double, rBpower: Double, lBpower: Double, milis: Int) {
         fourMotors(rFpower, lFpower, rBpower, lBpower)
         sleep(milis.toLong())
@@ -35,27 +39,28 @@ open class AimBotMovement(): AimBotHardware() {
         lFDrive?.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         rFDrive?.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         rBDrive?.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-//        lBDrive?.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        lBDrive?.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
 
 //        set motors to run toward for encoder counts.
         lFDrive?.targetPosition = target
         rFDrive?.targetPosition = target
         rBDrive?.targetPosition = target
-//        lBDrive?.targetPosition = target
+        lBDrive?.targetPosition = target
 
 //        set motors to run to target encoder position and stop with brakes on.
         lFDrive?.mode = DcMotor.RunMode.RUN_TO_POSITION
         rFDrive?.mode = DcMotor.RunMode.RUN_TO_POSITION
         rBDrive?.mode = DcMotor.RunMode.RUN_TO_POSITION
-//        lBDrive?.mode = DcMotor.RunMode.RUN_TO_POSITION
+        lBDrive?.mode = DcMotor.RunMode.RUN_TO_POSITION
     }
 
     fun moveEncoder(rFpower: Double, lFpower: Double, rBpower: Double, lBpower: Double, ticks: Int) {
-        fourMotors(rFpower, lFpower, rBpower, 0.0/*lBpower*/)
+        fourMotors(rFpower, lFpower, rBpower, lBpower)
         setPos(ticks)
         while (!rFDrive!!.isBusy && !lFDrive!!.isBusy && !rBDrive!!.isBusy /*&& !lBDrive!!.isBusy*/) {
             print("rF = ${rFDrive?.currentPosition} n/ lF = ${lFDrive?.currentPosition} n/ rB = ${rBDrive?.currentPosition} n/ lB = ${lBDrive?.currentPosition} n/ ")
         }
+        stopMotors()
     }
 
     fun eStraight(power: Double, ticks: Int) {
