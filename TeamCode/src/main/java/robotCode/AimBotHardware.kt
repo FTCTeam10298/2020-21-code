9c2f7c021fc892bc56002909117302c1babe812a
james <sj2@penrose.us>
2020-10-29 19:44:48 -0700

    set aimbot motors to brake

--- TeamCode/src/main/java/robotCode/AimBotHardware.kt
+++ TeamCode/src/main/java/robotCode/AimBotHardware.kt
@@ -32,5 +32,10 @@ open class AimBotHardware {
         lFDrive?.mode = DcMotor.RunMode.RUN_USING_ENCODER
         rBDrive?.mode = DcMotor.RunMode.RUN_USING_ENCODER
         lBDrive?.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        
+        rFDrive?.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        lFDrive?.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        rBDrive?.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        rBDrive?.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
     }
 }
