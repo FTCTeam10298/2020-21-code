ebf0db21a25cd50bb9986769dbd7596d2c618f40
james <sj2@penrose.us>
2020-11-04 08:05:27 -0800

    adjusted hardware for new srafer chassis

--- TeamCode/src/main/java/robotCode/AimBotHardware.kt
+++ TeamCode/src/main/java/robotCode/AimBotHardware.kt
@@ -33,32 +33,33 @@ open class AimBotHardware {
 
         hwMap = ahwMap
 
-//        DRIVE TRAIN
-        lFDrive = hwMap.get("lFDrive") as DcMotor
-        rFDrive = hwMap.get("rFDrive") as DcMotor
-        lBDrive = hwMap.get("lBDrive") as DcMotor
-        rBDrive = hwMap.get("rBDrive") as DcMotor
-
-        rFDrive.direction = DcMotorSimple.Direction.REVERSE
-        lFDrive.direction = DcMotorSimple.Direction.FORWARD
-        rBDrive.direction = DcMotorSimple.Direction.FORWARD
-        lBDrive.direction = DcMotorSimple.Direction.REVERSE
-
-        rFDrive.mode = DcMotor.RunMode.RUN_USING_ENCODER
-        lFDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-        rBDrive.mode = DcMotor.RunMode.RUN_USING_ENCODER
-        lBDrive.mode = DcMotor.RunMode.RUN_USING_ENCODER
-
-        rFDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-        lFDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-        rBDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-        rBDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-
 //        SHOOTER
         shooter = hwMap.get("lFDrive") as DcMotor
 
         shooter.direction = DcMotorSimple.Direction.REVERSE
         shooter.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
         shooter.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
+
+//        DRIVE TRAIN
+        lFDrive = hwMap.get("lFDrive") as DcMotor
+        rFDrive = hwMap.get("rFDrive") as DcMotor
+        lBDrive = hwMap.get("lBDrive") as DcMotor
+        rBDrive = hwMap.get("rBDrive") as DcMotor
+
+        rFDrive.direction = DcMotorSimple.Direction.FORWARD
+        lFDrive.direction = DcMotorSimple.Direction.REVERSE
+        rBDrive.direction = DcMotorSimple.Direction.FORWARD
+        lBDrive.direction = DcMotorSimple.Direction.REVERSE
+
+        rFDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        lFDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        rBDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        lBDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+
+        rFDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        lFDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        rBDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        lBDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
     }
 }
