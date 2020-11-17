e624df19a32d1dcfc39324b7139330e8f1dc14ee
james <sj2@penrose.us>
2020-11-16 17:44:09 -0800

    fixed toggle

--- TeamCode/src/main/java/buttonHelper/ButtonHelper.kt
+++ TeamCode/src/main/java/buttonHelper/ButtonHelper.kt
@@ -11,21 +11,9 @@ class ButtonHelper() {
         return re
     }
 
-    fun loopButton(button: Boolean): Boolean {
-        return button
-    }
-
-    fun toggleButton(button: Boolean): Boolean {
+    fun toggle(button: Boolean): Boolean {
         return stateChanged(button) && button
     }
-
-//    fun hi() {
-//        when (toggleButton(gamepad.x)) {
-//            true -> println("button toggle 1")
-//            false -> println("button toggle 2")
-//        }
-//    }
-
 }
 
 
