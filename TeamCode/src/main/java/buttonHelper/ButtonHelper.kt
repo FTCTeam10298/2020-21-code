06d561e8236eda18a2607994067cf8fb2a7645fd
james <sj2@penrose.us>
2020-12-14 15:21:51 -0800

    made stick helper

--- TeamCode/src/main/java/buttonHelper/ButtonHelper.kt
+++ TeamCode/src/main/java/buttonHelper/ButtonHelper.kt
@@ -11,6 +11,18 @@ class ButtonHelper() {
         return re
     }
 
+    var stickPreviousValue = 0.0
+    fun stateChanged(button: Double): Boolean {
+        return if (stickPreviousValue > 0.0 && button > 0.0) {
+            false
+        } else {
+            val re: Boolean = stickPreviousValue != button
+            stickPreviousValue = button
+            re
+        }
+
+    }
+
     fun toggle(button: Boolean): Boolean {
         return stateChanged(button) && button
     }
