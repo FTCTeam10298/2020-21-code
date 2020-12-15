609675d09309cf0ffb8b005054d7605e8833921c
james <sj2@penrose.us>
2020-12-14 22:55:10 -0800

    created option system

--- TeamCode/src/main/java/jamesTelemetryMenu/PairSystem.kt
+++ TeamCode/src/main/java/jamesTelemetryMenu/PairSystem.kt
@@ -1,8 +1,8 @@
 package jamesTelemetryMenu
 
-open class PairSystem {
+class PairSystem {
 
-    /*private*/ val matches: MutableList<String> = mutableListOf("")
+    val matches: MutableList<String> = mutableListOf("")
 
     fun addPair(item1: String, item2: String) {
         matches.add(item1 + item2)
@@ -13,23 +13,41 @@ open class PairSystem {
     fun getMatches(predicate: String): String = getRaw(predicate).replace(predicate,"").removePrefix("[").removeSuffix("]")
 }
 
-open class OptionSystem: PairSystem() {
+class OptionSystem {
+
+    private val options = PairSystem()
+    private val items = PairSystem()
+
+    fun addOption(item: String, option: String) {
+        val id: Int = items.matches.size
+
+        if (options.matches.contains(option)) {
+            items.addPair(option, options.getMatches(option))
+        } else {
+            options.addPair(item, id.toString())
+            items.addPair(option, id.toString())
+        }
+    }
+
+    fun getItems(option: String): String = items.getMatches(options.getMatches(option))
+
+    fun listOfOptions(): MutableList<String> = options.matches
 
 }
 
-open class MenuChoices {
-    /*private*/ val choices = OptionSystem()
+open class MenuSystem {
+    /*private*/ val options = OptionSystem()
     private val links = PairSystem()
 
-    fun addChoiceItem(Choice: String, item: String) {
-        choices.addPair(Choice, item)
+    fun addChoiceItem(option: String, item: String) {
+        options.addOption(option, item)
     }
+
+    fun getItems(option: String): String = options.getItems(option)
 
     fun addLink(item: String, Choice: String) {
         links.addPair(item, Choice)
     }
-
-    fun getOptions(choice: String): String = choices.getMatches(choice)
 
     fun getLink(item: String): String = links.getMatches(item)
 }
