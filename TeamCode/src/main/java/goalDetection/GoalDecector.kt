a45453eee8308e632ce6f7cc829f31a2c32107ec
james <sj2@penrose.us>
2021-01-05 13:22:54 -0800

    first try at new trap algorithm

--- TeamCode/src/main/java/goalDetection/GoalDecector.kt
+++ TeamCode/src/main/java/goalDetection/GoalDecector.kt
@@ -38,7 +38,7 @@ class GoalDetector(private val console: TelemetryConsole) {
     fun detectTrapezoid(frame: Mat) {
 
         val blurredFrame = Mat()
-        Imgproc.GaussianBlur(frame, blurredFrame, Size(5.0, 5.0), 0.0)
+        Imgproc.GaussianBlur(frame, blurredFrame, Size(10.0, 10.0), 0.0)
 
 //        To be tuned
         val lowBlue = doubleArrayOf(48.0, 86.0, 0.0)
@@ -46,15 +46,22 @@ class GoalDetector(private val console: TelemetryConsole) {
 
         val mask = colorMask(blurredFrame, lowBlue, highBlue)
 
-        
+//        val erodedMask = Mat()
+//        Imgproc.erode(mask, erodedMask, Mat())
+
         val contours: MutableList<MatOfPoint> = mutableListOf()
         Imgproc.findContours(mask, contours, Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE)
 
-        val largeContours = contours.filter{Imgproc.contourArea(it) > 100}
-        largeContours.forEach{ Imgproc.drawContours(frame, listOf(it), 0, Scalar(0.0, 255.0, 0.0), 2) }
+        val largeContours = contours.filter{ Imgproc.contourArea(it) > 500}
+        val squareContours = largeContours.filter{ isSquare(it) }
+        squareContours.forEach {
 
-        val squareContours = largeContours.filter{isSquare(it)}
-        squareContours.forEach{ Imgproc.circle(frame, contourCenter(it), 3, Scalar(255.0, 255.0, 255.0), -1) }
+//            val x = it.row(0)
+//            val y = it.row(1)
+            Imgproc.drawContours(frame, listOf(it), 0, Scalar(0.0, 255.0, 0.0), 2)
+            Imgproc.circle(frame, contourCenter(it), 3, Scalar(255.0, 255.0, 255.0), -1)
+        }
+
 
         display = frame
     }
@@ -69,19 +76,19 @@ class GoalDetector(private val console: TelemetryConsole) {
         return mask
     }
 
-    private fun isSquare(contour: MatOfPoint): Boolean {
-        val cnt = MatOfPoint2f()
-        contour.convertTo(cnt, CvType.CV_32F)
+    private fun smoothContour(contour: MatOfPoint): MatOfPoint {
+        val cnt = contour.toMatOfPoint2f()
 
         val peri = Imgproc.arcLength(cnt, true)
-        val approx = MatOfPoint2f()
-        Imgproc.approxPolyDP(cnt, approx,0.01 * peri, true)
 
-        val a = MatOfPoint()
-        approx.convertTo(a, CvType.CV_32S)
+        val smoothed = MatOfPoint2f()
+        Imgproc.approxPolyDP(cnt, smoothed,0.01 * peri, true)
 
-        return a.rows() == 4
+
+        return smoothed.toMatOfPoint()
     }
+
+    private fun isSquare(contour: MatOfPoint): Boolean = smoothContour(contour).rows() == 4
 
     private fun contourCenter(contour: MatOfPoint): Point {
         val m: Moments = Imgproc.moments(contour)
@@ -90,4 +97,16 @@ class GoalDetector(private val console: TelemetryConsole) {
 
         return Point(cX, cY)
     }
+}
+
+private fun MatOfPoint.toMatOfPoint2f(): MatOfPoint2f {
+    val matOfPoint2f = MatOfPoint2f()
+    this.convertTo(matOfPoint2f, CvType.CV_32F)
+    return matOfPoint2f
+}
+
+private fun MatOfPoint2f.toMatOfPoint(): MatOfPoint {
+    val matOfPoint = MatOfPoint()
+    this.convertTo(matOfPoint, CvType.CV_32S)
+    return matOfPoint
 }
