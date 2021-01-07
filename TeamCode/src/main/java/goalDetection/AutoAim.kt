83eb63ae8c8a36747d74fb771bed1df918a79504
james <sj2@penrose.us>
2021-01-07 15:03:07 -0800

    started structure of auto aim

--- TeamCode/src/main/java/goalDetection/AutoAim.kt
+++ TeamCode/src/main/java/goalDetection/AutoAim.kt
@@ -4,14 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import jamesTelemetryMenu.TelemetryConsole
 import locationTracking.Coordinate
+import robotCode.aimBotRobot.EncoderDriveMovement
 
 @TeleOp
-class AutoAim(private val console: TelemetryConsole, opMode: OpMode) {
+class AutoAim(private val console: TelemetryConsole, opMode: OpMode, val robot: EncoderDriveMovement) {
 
     val detector = GoalDetector(console)
     val opencv = OpencvAbstraction(opMode)
     var aimed = false
-    val coordinate = Coordinate()
+    val robotPos = Coordinate(0.0)
+    val goalPos = Coordinate(21.0)
 
     fun init() {
         opencv.init()
@@ -20,10 +22,15 @@ class AutoAim(private val console: TelemetryConsole, opMode: OpMode) {
 
     fun aim() {
 
+//        Turn toward goal
+        while (goalPos.r !== robotPos.r)
+            robot.driveRobotTurn(1.0, 1.0)
+
+//        fine tune aim
         while (!aimed) {
             detector.detectTrapezoid(opencv.frame)
             detector.goal
-            coordinate.
+//            if (detector.x = )
         }
 
     }
