fe4e8e8edf6f91f1efbb322105200b9c64d85a37
DockingBay-94 <pbryson3.1415@gmail.com>
2021-01-07 19:10:19 -0800

    updated to current hardware

--- TeamCode/src/main/java/robotCode/AimBotTeleOp.kt
+++ TeamCode/src/main/java/robotCode/AimBotTeleOp.kt
@@ -1,7 +1,6 @@
 package robotCode
 
 //import buttonHelper.Gamepad1
-import android.os.SystemClock.sleep
 import buttonHelper.ButtonHelper
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
@@ -13,7 +12,8 @@ import kotlin.math.pow
 @TeleOp(name="Aim Bot Tele-Op", group="Aim Bot")
 class AimBotTeleOp(): OpMode() {
 
-    val robot = MecanumDriveTrain()
+    val hardware = AimBotHardware()
+    val robot = MecanumDriveTrain(hardware)
     val console = TelemetryConsole(telemetry)
 
     var driveDirection: Int = 1
@@ -29,7 +29,7 @@ class AimBotTeleOp(): OpMode() {
 
     override fun init() {
         console.display(1, "Initializing...")
-        robot.init(hardwareMap)
+        hardware.init(hardwareMap)
         console.display(1, "Initialized")
     }
 
@@ -62,74 +62,74 @@ class AimBotTeleOp(): OpMode() {
 
 //        Shoot routine
         if (gamepad1.right_trigger > 0.2) {
-            robot.shooter.setVelocityPIDFCoefficients(55.0, 1.0, 0.0,0.0)
-            robot.shooter.velocity = 1980.0
-            if (robot.shooter.velocity * 60 / 28 >= 4114.0 /*RPM*/ ) {
-                robot.gate.position = 1.0
-                robot.belt.power = 0.8
+            hardware.shooter.setVelocityPIDFCoefficients(55.0, 1.0, 0.0,0.0)
+            hardware.shooter.velocity = 1980.0
+            if (hardware.shooter.velocity * 60 / 28 >= 4114.0 /*RPM*/ ) {
+                hardware.gate.position = 1.0
+                hardware.belt.power = 0.8
             }
         } else {
-            robot.belt.power = 0.0
+            hardware.belt.power = 0.0
 //            robot.shooter.power = 1.0   // Idle Shooter
-            robot.gate.position = 0.0
+            hardware.gate.position = 0.0
         }
-        console.display(4, "Shooter rpm: ${robot.shooter.velocity * 60 / 28}")
-        console.display(5, "Shooter Power: ${robot.shooter.power}")
+        console.display(4, "Shooter rpm: ${hardware.shooter.velocity * 60 / 28}")
+        console.display(5, "Shooter Power: ${hardware.shooter.power}")
 
 //        SHOOTER
         val shooterPowerIncrement: Double = 0.008
-        val shooterPower: Double = robot.shooter.power
+        val shooterPower: Double = hardware.shooter.power
 
         when {
-            (dUpHelp.stateChanged(gamepad1.dpad_up) && gamepad1.dpad_up) && shooterPower < 1.0 -> robot.shooter.power += shooterPowerIncrement
-            (dDownHelp.stateChanged(gamepad1.dpad_down) && gamepad1.dpad_down) && shooterPower > 0.0 + shooterPowerIncrement -> robot.shooter.power -= shooterPowerIncrement
-            gamepad1.dpad_left -> robot.shooter.power = 0.0
-            gamepad1.dpad_right -> robot.shooter.power = 0.83
+            (dUpHelp.stateChanged(gamepad1.dpad_up) && gamepad1.dpad_up) && shooterPower < 1.0 -> hardware.shooter.power += shooterPowerIncrement
+            (dDownHelp.stateChanged(gamepad1.dpad_down) && gamepad1.dpad_down) && shooterPower > 0.0 + shooterPowerIncrement -> hardware.shooter.power -= shooterPowerIncrement
+            gamepad1.dpad_left -> hardware.shooter.power = 0.0
+            gamepad1.dpad_right -> hardware.shooter.power = 0.83
         }
 
 //        COLLECTOR
         if (collectorHelp1.stateChanged(gamepad1.right_bumper) && (gamepad1.right_bumper))
-            if (robot.collector.power == 1.0)
-                robot.collector.power = 0.0
+            if (hardware.collector.power == 1.0)
+                hardware.collector.power = 0.0
             else
-                robot.collector.power = 1.0
+                hardware.collector.power = 1.0
         else if (collectorHelp2.stateChanged(gamepad1.left_bumper) && (gamepad1.left_bumper))
-            if (robot.collector.power == -1.0)
-                robot.collector.power = 0.0
+            if (hardware.collector.power == -1.0)
+                hardware.collector.power = 0.0
             else
-                robot.collector.power = -1.0
+                hardware.collector.power = -1.0
 
 //        BELT
         when {
-            gamepad1.b || gamepad2.b -> robot.belt.power = 0.8
-            gamepad1.b || gamepad2.b -> robot.belt.power = -0.8
-            else -> robot.belt.power = 0.0
+            gamepad1.b || gamepad2.b -> hardware.belt.power = 0.8
+            gamepad1.b || gamepad2.b -> hardware.belt.power = -0.8
+            else -> hardware.belt.power = 0.0
         }
 
 
 //        WOBBLE ARM
         val wobbleStick = gamepad2.right_stick_y
-        robot.wobbleArm.power = wobbleStick.toDouble()
+        hardware.wobbleArm.power = wobbleStick.toDouble()
 
 //        CLAW
         if (clawHelp.stateChanged(gamepad2.x) && gamepad2.x)
-            when (robot.claw.position) {
-                0.0  -> robot.claw.position = 1.0
-                else -> robot.claw.position = 0.0
+            when (hardware.claw.position) {
+                0.0  -> hardware.claw.position = 1.0
+                else -> hardware.claw.position = 0.0
             }
 
 //        CONSOLE
 //        console.display(5, "Collector: ${robot.collector.power}")
-        console.display(6, "Claw: ${robot.claw.position}")
-        console.display(7, "Belt: ${robot.belt.power}")
+        console.display(6, "Claw: ${hardware.claw.position}")
+        console.display(7, "Belt: ${hardware.belt.power}")
         when {
             driveDirection > 0 -> console.display(8, "Collector first")
             else -> console.display(8, "Shooter first")
         }
-        console.display(9, "LF: ${robot.lFDrive.power}")
-        console.display(10, "RF: ${robot.rFDrive.power}")
-        console.display(11, "LB: ${robot.lBDrive.power}")
-        console.display(12, "RB: ${robot.rBDrive.power}")
+        console.display(9, "LF: ${hardware.lFDrive.power}")
+        console.display(10, "RF: ${hardware.rFDrive.power}")
+        console.display(11, "LB: ${hardware.lBDrive.power}")
+        console.display(12, "RB: ${hardware.rBDrive.power}")
     }
 
     fun pow(n: Double, exponent: Double): Double {
