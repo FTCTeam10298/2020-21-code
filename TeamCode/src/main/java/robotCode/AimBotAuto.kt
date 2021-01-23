7a99ab2a798ac7713e6e27b1b6e7d5c0703ebebf
glaferg <gabefergesen@gmail.com>
2021-01-22 19:30:51 -0800

    fart button

--- TeamCode/src/main/java/robotCode/AimBotAuto.kt
+++ TeamCode/src/main/java/robotCode/AimBotAuto.kt
@@ -58,20 +58,8 @@ class AimBotAuto: LinearOpMode() {
 //        robot.driveRobotPosition(1.0, 52.0, true)
 //        }
 //        NewRingDetector.RingPosition.ONE -> {
-            robot.driveRobotPosition(1.0, -95.0, true)
-            robot.driveRobotTurn(0.5, 180.0)
-            hardware.wobbleArm.power = 0.8
-            sleep(1000)
-            hardware.wobbleArm.power = 0.0
-            hardware.lClaw.position = 0.0; hardware.rClaw.position = 0.0
-            hardware.wobbleArm.power = -0.8
-            sleep(1000)
-            hardware.wobbleArm.power = 0.0
-            robot.driveRobotPosition(1.0, -31.0, true)
-            robot.driveRobotTurn(0.5, -180.0)
-//        }
-//        NewRingDetector.RingPosition.NONE -> {
-//            robot.driveRobotPosition(1.0, -79.0, true)
+//            robot.driveRobotPosition(1.0, -95.0, true)
+//            robot.driveRobotTurn(0.5, 180.0)
 //            hardware.wobbleArm.power = 0.8
 //            sleep(1000)
 //            hardware.wobbleArm.power = 0.0
@@ -79,6 +67,21 @@ class AimBotAuto: LinearOpMode() {
 //            hardware.wobbleArm.power = -0.8
 //            sleep(1000)
 //            hardware.wobbleArm.power = 0.0
+//            robot.driveRobotPosition(1.0, -31.0, true)
+//            robot.driveRobotTurn(0.5, -180.0)
+//        }
+//        NewRingDetector.RingPosition.NONE -> {
+            robot.driveRobotPosition(1.0, -79.0, true)
+            hardware.wobbleArm.power = 0.8
+            sleep(1000)
+            hardware.wobbleArm.power = 0.0
+            hardware.lClaw.position = 0.0; hardware.rClaw.position = 0.0
+            hardware.wobbleArm.power = -0.8
+            sleep(1000)
+            hardware.wobbleArm.power = 0.0
+            robot.driveRobotPosition(1.0, -12.0, true)
+
+
 //        }
 
         console.display(1, "Cupertino, The Wobble Is Down ${ringDetector.position}")
