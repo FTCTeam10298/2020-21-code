763d30e4b50b823ebe57659f7017d15243088c4c
james <sj2@penrose.us>
2021-01-22 22:14:47 -0800

    Revert scuffed camera changes

--- TeamCode/src/main/java/goalDetection/OpencvAbstraction.kt
+++ TeamCode/src/main/java/goalDetection/OpencvAbstraction.kt
@@ -2,15 +2,8 @@ package goalDetection
 
 import android.os.SystemClock.sleep
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
-import org.firstinspires.ftc.robotcontroller.external.samples.ConceptWebcam
-import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap
-import org.firstinspires.ftc.robotcore.external.ClassFactory
-import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraManager
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
-import org.firstinspires.ftc.robotcore.internal.system.Deadline
 import org.opencv.core.Mat
 import org.openftc.easyopencv.*
-import java.util.concurrent.TimeUnit
 
 //class CameraMaker {
 //
@@ -58,23 +51,16 @@ class OpencvAbstraction(private val opmode: OpMode) {
     private val pipeline = PipelineAbstraction()
     val frame = Mat()
 
-    private lateinit var cameraManager: CameraManager
-    private lateinit var camera: org.firstinspires.ftc.robotcore.external.hardware.camera
-    private lateinit var webcamName: WebcamName
+    private lateinit var camera: OpenCvInternalCamera
     var cameraDirection: OpenCvInternalCamera.CameraDirection = OpenCvInternalCamera.CameraDirection.FRONT
     var optimizeView = false
     var openCameraDeviceAsync = false
 
 
     fun init() {
-
-        cameraManager = ClassFactory.getInstance().cameraManager
-
         val cameraMonitorViewId: Int = opmode.hardwareMap.appContext.resources.getIdentifier("cameraMonitorViewId", "id", opmode.hardwareMap.appContext.packageName)
-
-        webcamName = hardwareMap.get(WebcamName::class.java, "Webcam 1")
-        val deadline = Deadline(ConceptWebcam.secondsPermissionTimeout.toLong(), TimeUnit.SECONDS)
-        camera = cameraManager.requestPermissionAndOpenCamera(deadline, cameraName, null);
+        camera = OpenCvCameraFactory.getInstance().createInternalCamera(cameraDirection, cameraMonitorViewId)
+        camera.openCameraDevice()
         camera.setPipeline(pipeline)
     }
 
