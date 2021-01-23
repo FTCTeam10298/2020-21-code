b48b66017f96a8d1db37d76a547fd2f41150fb2c
james <sj2@penrose.us>
2021-01-23 10:27:05 -0800

    adjusted comments

--- TeamCode/src/main/java/robotCode/AimBotAuto.kt
+++ TeamCode/src/main/java/robotCode/AimBotAuto.kt
@@ -45,19 +45,20 @@ class AimBotAuto: LinearOpMode() {
 
         opencv.stop()
 
-        when (ringDetector.position) {
-            RingDetector.RingPosition.FOUR -> { // Step 1 deliver wobble
-                robot.driveRobotPosition(1.0, -126.0, true)
-                hardware.wobbleArm.power = 0.8
-                sleep(1000)
-                hardware.wobbleArm.power = 0.0
-                hardware.lClaw.position = 0.0; hardware.rClaw.position = 0.0
-                hardware.wobbleArm.power = -0.8
-                sleep(1000)
-                hardware.wobbleArm.power = 0.0
-                robot.driveRobotPosition(1.0, 52.0, true) // Step 2 shoot power shots
-            }
-            RingDetector.RingPosition.ONE -> { // Step 1 deliver wobble
+//        when (ringDetector.position) {
+//            RingDetector.RingPosition.FOUR -> { // Step 1 deliver wobble
+//                robot.driveRobotPosition(1.0, -126.0, true)
+//                hardware.wobbleArm.power = 0.8
+//                sleep(1000)
+//                hardware.wobbleArm.power = 0.0
+//                hardware.lClaw.position = 0.0; hardware.rClaw.position = 0.0
+//                hardware.wobbleArm.power = -0.8
+//                sleep(1000)
+//                hardware.wobbleArm.power = 0.0
+//                robot.driveRobotPosition(1.0, 52.0, true) // Step 2 shoot power shots
+//            }
+//            RingDetector.RingPosition.ONE -> {
+                // Step 1 deliver wobble
                 robot.driveRobotPosition(1.0, -95.0, true)
                 robot.driveRobotTurn(0.5, 180.0)
                 hardware.wobbleArm.power = 0.8
@@ -68,8 +69,9 @@ class AimBotAuto: LinearOpMode() {
                 sleep(1000)
                 hardware.wobbleArm.power = 0.0
                 robot.driveRobotPosition(1.0, -31.0, true)
-                robot.driveRobotTurn(0.5, -180.0) // Step 2 shoot power shots
-                console.display(1, "Cupertino, The Wobble Is Down ${ringDetector.position}")
+                robot.driveRobotTurn(0.5, -180.0)
+
+                // Step 2 shoot power shots
                 robot.driveSidewaysTime(1.0, 1.0)
                 hardware.collector.power = 1.0
                 shoot()
@@ -78,19 +80,19 @@ class AimBotAuto: LinearOpMode() {
                 robot.driveRobotTurn(0.5, -22.5)
                 shoot()
                 robot.driveRobotTurn(0.5, 11.25)
-            }
-            RingDetector.RingPosition.NONE -> { // Step 1 deliver wobble
-                robot.driveRobotPosition(1.0, -79.0, true)
-                hardware.wobbleArm.power = 0.8
-                sleep(1000)
-                hardware.wobbleArm.power = 0.0
-                hardware.lClaw.position = 0.0; hardware.rClaw.position = 0.0
-                hardware.wobbleArm.power = -0.8
-                sleep(1000)
-                hardware.wobbleArm.power = 0.0
-                robot.driveRobotPosition(1.0, 12.0, true)
-            }
-        }
+//            }
+//            RingDetector.RingPosition.NONE -> { // Step 1 deliver wobble
+//                robot.driveRobotPosition(1.0, -79.0, true)
+//                hardware.wobbleArm.power = 0.8
+//                sleep(1000)
+//                hardware.wobbleArm.power = 0.0
+//                hardware.lClaw.position = 0.0; hardware.rClaw.position = 0.0
+//                hardware.wobbleArm.power = -0.8
+//                sleep(1000)
+//                hardware.wobbleArm.power = 0.0
+//                robot.driveRobotPosition(1.0, 12.0, true)
+//            }
+//        }
 
 
 
