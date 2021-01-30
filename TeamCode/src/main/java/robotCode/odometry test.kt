8725e6856c38a9b71f4739c90b4587d60837bbc6
james <sj2@penrose.us>
2021-01-29 19:35:49 -0800

    odometry test

---
+++ TeamCode/src/main/java/robotCode/odometry test.kt
@@ -0,0 +1,14 @@
+package robotCode
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+class odometryTest: LinearOpMode() {
+
+
+    
+    override fun runOpMode() {
+
+        waitForStart()
+
+    }
+}
