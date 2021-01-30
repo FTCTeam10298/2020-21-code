00609ca3588fc40f2b0bf8c882bf0d614c5f5d7d
james <sj2@penrose.us>
2021-01-29 16:51:35 -0800

    uncommented object recognition

--- TeamCode/src/main/java/goalDetection/ObjectRecognition.kt
+++ TeamCode/src/main/java/goalDetection/ObjectRecognition.kt
@@ -1,13 +1,12 @@
-//package goalDetection
-//
-//import org.opencv.core.Core
-//import org.opencv.imgcodecs.Imgcodecs
-//import org.opencv.imgproc.Imgproc
-//
-//class ObjectRecognition {
-//
-//    val objects = listOf("redGoal", "blueGoal", "goal", "else")
-//
-//    val
-//
-//}
+package goalDetection
+
+import org.opencv.core.Core
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
+
+class ObjectRecognition {
+
+    val objects = listOf("redGoal", "blueGoal", "goal", "else")
+
+
+}
