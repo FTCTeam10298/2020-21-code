d15186bb6023e53463240db118a5d98a92953b18
glaferg <gabefergesen@gmail.com>
2021-02-20 10:10:32 -0800

    Gabe's Weird Python CV: Masking...

--- creamsicle-vision2/scoop.py
+++ creamsicle-vision2/scoop.py
@@ -1,16 +1,45 @@
-import numpy as np
 import cv2
-import drizzle
+import numpy as np
 
-cap = cv2.VideoCapture(0)
+
+def nothing(x):
+    # any operation
+    pass
+
+
+cap = cv2.VideoCapture(2)
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 255, 255, nothing)
 
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow('frame', gray)
+    l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L-S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L-V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U-S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U-V", "Trackbars")
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    lower_red = np.array([l_h, l_s, l_v])
+    upper_red = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    #Contours Detection
+
+    cv2.imshow('Frame', frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(1)
+    if key == 27:
         break
 
 cap.release()
