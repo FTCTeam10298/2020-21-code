7a8e9ec4c0eef47256efc82e797d21c22809ca94
DockingBay-94 <pbryson3.1415@gmail.com>
2021-03-12 19:37:08 -0800

    odometryHardware moved to its owen file

---
+++ TeamCode/src/main/java/robotCode/OdometryHardware.kt
@@ -0,0 +1,57 @@
+package robotCode
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+import robotCode.hardwareClasses.MecOdometryHardware
+
+class odometryHardware: MecOdometryHardware {
+    override lateinit var lOdom: DcMotor
+    override lateinit var rOdom: DcMotor
+    override lateinit var cOdom: DcMotor
+    override lateinit var lFDrive: DcMotor
+    override lateinit var rFDrive: DcMotor
+    override lateinit var lBDrive: DcMotor
+    override lateinit var rBDrive: DcMotor
+
+    override lateinit var hwMap: HardwareMap
+
+    override fun init(ahwMap: HardwareMap) {
+
+        hwMap = ahwMap
+
+        lFDrive = hwMap.get("left drive f") as DcMotor
+        lBDrive = hwMap.get("left drive b") as DcMotor
+        rFDrive = hwMap.get("right drive f") as DcMotor
+        rBDrive = hwMap.get("right drive b") as DcMotor
+
+        rFDrive.direction = DcMotorSimple.Direction.FORWARD
+        lFDrive.direction = DcMotorSimple.Direction.REVERSE
+        rBDrive.direction = DcMotorSimple.Direction.FORWARD
+        lBDrive.direction = DcMotorSimple.Direction.REVERSE
+
+        rFDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        lFDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        rBDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        lBDrive.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+
+        rFDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        lFDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        rBDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        lBDrive.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
+
+        lOdom = hwMap.dcMotor.get("left collector")
+        cOdom = hwMap.dcMotor.get("tape")
+        rOdom = hwMap.dcMotor.get("left drive b")
+
+        lOdom.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        lOdom.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+
+        cOdom.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        cOdom.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+
+        rOdom.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        rOdom.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+    }
+}
