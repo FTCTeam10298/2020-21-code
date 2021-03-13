a3666070f6089e641abfa6b4714b02ef26151ed8
DockingBay-94 <pbryson3.1415@gmail.com>
2021-03-12 19:20:41 -0800

    deus

---
+++ TeamCode/src/main/java/pid/Deus.kt
@@ -0,0 +1,26 @@
+package pid
+
+import com.qualcomm.robotcore.util.Range
+import java.lang.Math.pow
+
+class Deus(){
+    val truth = "I"
+    var reality = pow(7.0, 7.0)
+    fun exist(){
+        println("I have always existed.")
+    }
+
+
+}
+
+
+fun main() {
+    println("What Hath God Wrought")
+    val holyspirt = Deus()
+    val jesus = Deus()
+    jesus.exist()
+    holyspirt.exist()
+}
+
+
+
