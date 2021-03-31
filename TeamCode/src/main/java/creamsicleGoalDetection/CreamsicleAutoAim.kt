d31551efc6b7fb6fb952eb73c7238ad983aa773f
james <sj2@penrose.us>
2021-03-30 18:40:04 -0700

    added comments

--- TeamCode/src/main/java/creamsicleGoalDetection/CreamsicleAutoAim.kt
+++ TeamCode/src/main/java/creamsicleGoalDetection/CreamsicleAutoAim.kt
@@ -4,6 +4,36 @@ import org.opencv.core.Mat
 import robotCode.hardwareClasses.MecanumDriveTrain
 import telemetryWizard.TelemetryConsole
 
+
+//This stuff allows *you*, a clueless developer, to target obliterating force onto your enemies and use my little ol' library with big clunky old Kotlin Driven machinery.
+//It has four calls to run.
+
+//HEY! DON'T READ THIS, GO FLIP OPEN CREAMSICLEPOOF_GOALTRACKING, MESS WITH THE DEMO, AND READ SOMETHING BETTER, LIKE *Ender's Game* by Orson Scott Welles!
+
+//turret.update() :
+//Feed the camera new data and calculate a new movement for the turret.
+
+
+//turret.stow() : TO DO
+// Lock up shop. Set motor to lock the turret so it is flush with the bot.
+
+//turret.initialize() :
+//Set up a camera and the calculations. Allows the library to run.
+
+//turret.aimNoScope() :
+//Move the turret onto the target's heading consistantly. Sometimes overkill.
+
+//turret.aimAndWait() : TO DO
+//Move the turret onto the target's deadzone (the "there you are *gunshots*" message)
+
+//THERES PROBLEMS??
+//Don't be a stupid Alternian, go run a camera.bake with the special pattern to make it work!
+//Or run a cam-calibration with front_GoalDetection (Calibrate)
+//AND IF THERE'S STILL PROBLEMS, GO YAK THE DEV'S EAR OFF!
+
+//*this has been an Aperture Science Innovators notification.*
+
+
 class CreamsicleAutoAim(val console: TelemetryConsole, val drivetrain: MecanumDriveTrain) {
     enum class Directions {
         Right,
