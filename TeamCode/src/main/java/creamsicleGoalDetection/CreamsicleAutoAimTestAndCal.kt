39fa2bd69a97cd8095e551c274841477ae12c5d0
james <sj2@penrose.us>
2021-04-28 21:05:24 -0700

    Revert "Finalized Color Selection and Fixed Stupid Color Names"
    This reverts commit b82bf1cb678c4539a7e675f4fe5662688b16b88b.

--- TeamCode/src/main/java/creamsicleGoalDetection/CreamsicleAutoAimTestAndCal.kt
+++ TeamCode/src/main/java/creamsicleGoalDetection/CreamsicleAutoAimTestAndCal.kt
@@ -1,118 +1,106 @@
-package creamsicleGoalDetection
-
-import buttonHelper.ButtonHelper
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import com.qualcomm.robotcore.eventloop.opmode.OpMode
-import openCvAbstraction.OpenCvAbstraction
-import org.opencv.imgproc.Imgproc
-import robotCode.ChoiVicoHardware
-import robotCode.hardwareClasses.EncoderDriveMovement
-import robotCode.hardwareClasses.OdometryDriveMovement
-import telemetryWizard.TelemetryConsole
-
-
-@Autonomous(name="Creamsicle Test and Calibration", group="Tests")
-class CreamsicleAutoAimTestAndCal : OpMode() {
-    val hardware = ChoiVicoHardware()
-    val console = TelemetryConsole(telemetry)
-    val robot = OdometryDriveMovement(console, hardware)
-    val opencv = OpenCvAbstraction(this)
-    val goalDetector = CreamsicleGoalDetector(console)
-    val aimer = UltimateGoalAimer(console, goalDetector, hardware)
-    val movement = EncoderDriveMovement(hardware, console)
-    val font = Imgproc.FONT_HERSHEY_COMPLEX
-    val XbuttonHelper = ButtonHelper()
-    val YbuttonHelper = ButtonHelper()
-    val DpadHelper = ButtonHelper()
-    val RbumperHelper = ButtonHelper()
-    val LbumperHelper = ButtonHelper()
-    val AbuttonHelper = ButtonHelper()
-
-    override fun init() {
-        hardware.init(hardwareMap)
-        goalDetector.targetHue = CreamsicleGoalDetector.TargetHue.RED
-        opencv.init(hardwareMap)
-        opencv.start()
-        opencv.onNewFrame(goalDetector::scoopFrame)
-    }
-
-    private var varBeingEdited: CreamsicleGoalDetector.NamedVar = goalDetector.redColor.L_H
-    fun render() {
-        console.display(2, "Active Var; ${varBeingEdited.name}")
-        console.display(4, "${varBeingEdited.value}")
-    }
-
-    override fun init_loop() {
-        if (XbuttonHelper.stateChanged(gamepad1.x) && gamepad1.x) {
-            console.display(3, "TrainerMODE; ${goalDetector.displayMode}")
-            when (goalDetector.displayMode) {
-                "frame" -> goalDetector.displayMode = "mask"
-                "mask" -> goalDetector.displayMode = "kernel"
-                "kernel" -> goalDetector.displayMode = "frame"
-            }
-            render()
-        }
-
-        //yo GRRL, set ur COLOR to whatever ud LIKE! Like redColor.L_H = redLow_Hue
-        when {
-            DpadHelper.stateChanged(gamepad1.dpad_left) && gamepad1.dpad_left -> {
-                when (varBeingEdited) {
-                    goalDetector.redColor.L_H -> varBeingEdited = goalDetector.redColor.L_S
-                    goalDetector.redColor.L_S -> varBeingEdited = goalDetector.redColor.L_V
-                    goalDetector.redColor.L_V -> varBeingEdited = goalDetector.redColor.U_H
-                    goalDetector.redColor.U_H -> varBeingEdited = goalDetector.redColor.U_S
-                    goalDetector.redColor.U_S -> varBeingEdited = goalDetector.redColor.U_V
-                    goalDetector.redColor.U_V -> varBeingEdited = goalDetector.redColor.L_H
-                }
-                render()
-            }
-        }
-
-        if (YbuttonHelper.stateChanged(gamepad1.y) && gamepad1.y) {
-            console.display(1, "Vals Zeroed")
-            goalDetector.redColor.L_H.value = 0.0
-            goalDetector.redColor.L_S.value = 0.0
-            goalDetector.redColor.L_V.value = 0.0
-            goalDetector.redColor.U_H.value = 0.0
-            goalDetector.redColor.U_S.value = 0.0
-            goalDetector.redColor.U_V.value = 0.0
-            goalDetector.blueColor.L_H.value = 0.0
-            goalDetector.blueColor.L_S.value = 0.0
-            goalDetector.blueColor.L_V.value = 0.0
-            goalDetector.blueColor.U_H.value = 255.0
-            goalDetector.blueColor.U_S.value = 255.0
-            goalDetector.blueColor.U_V.value = 255.0
-            render()
-        }
-
-        if (RbumperHelper.stateChanged(gamepad1.right_bumper) && gamepad1.right_bumper) {
-            varBeingEdited.value += 5
-            render()
-        }
-        if (LbumperHelper.stateChanged(gamepad1.left_bumper) && gamepad1.left_bumper) {
-            varBeingEdited.value -= 5
-            render()
-        }
-
-        if (AbuttonHelper.stateChanged(gamepad1.a) && gamepad1.a) {
-            goalDetector.redColor.L_H.value = 0.0
-            goalDetector.redColor.L_S.value = 0.0
-            goalDetector.redColor.L_V.value = 0.0
-            goalDetector.redColor.U_H.value = 255.0
-            goalDetector.redColor.U_S.value = 255.0
-            goalDetector.redColor.U_V.value = 255.0
-            goalDetector.blueColor.L_H.value = 0.0
-            goalDetector.blueColor.L_S.value = 0.0
-            goalDetector.blueColor.L_V.value = 0.0
-            goalDetector.blueColor.U_H.value = 255.0
-            goalDetector.blueColor.U_S.value = 255.0
-            goalDetector.blueColor.U_V.value = 255.0
-            console.display(1, "Vals Squonked")
-            render()
-        }
-    }
-
-    override fun loop() {
-        aimer.updateAimAndAdjustRobot()
-    }
-}
+//package creamsicleGoalDetection
+//
+//import buttonHelper.ButtonHelper
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode
+//import openCvAbstraction.OpenCvAbstraction
+//import org.opencv.imgproc.Imgproc
+//import robotCode.ChoiVicoHardware
+//import robotCode.hardwareClasses.EncoderDriveMovement
+//import robotCode.hardwareClasses.OdometryDriveMovement
+//import telemetryWizard.TelemetryConsole
+//
+//
+//@Autonomous(name="Creamsicle Test and Calibration", group="Tests")
+//class CreamsicleAutoAimTestAndCal : OpMode() {
+//    val hardware = ChoiVicoHardware()
+//    val console = TelemetryConsole(telemetry)
+//    val robot = OdometryDriveMovement(console, hardware)
+//    val opencv = OpenCvAbstraction(this)
+//    val goalDetector = CreamsicleGoalDetector(console)
+//    val aimer = UltimateGoalAimer(console, goalDetector, hardware)
+//    val movement = EncoderDriveMovement(hardware, console)
+//    val font = Imgproc.FONT_HERSHEY_COMPLEX
+//    val XbuttonHelper = ButtonHelper()
+//    val YbuttonHelper = ButtonHelper()
+//    val DpadHelper = ButtonHelper()
+//    val RbumperHelper = ButtonHelper()
+//    val LbumperHelper = ButtonHelper()
+//    val AbuttonHelper = ButtonHelper()
+//
+//    override fun init() {
+//        hardware.init(hardwareMap)
+//        opencv.init(hardwareMap)
+//        opencv.start()
+//        opencv.onNewFrame(goalDetector::scoopFrame)
+//    }
+//
+//    private var varBeingEdited: CreamsicleGoalDetector.NamedVar = goalDetector.L_H
+//    fun render() {
+//        console.display(2, "Active Var; ${varBeingEdited.name}")
+//        console.display(4, "${varBeingEdited.value}")
+//    }
+//
+//    override fun init_loop() {
+//        if (XbuttonHelper.stateChanged(gamepad1.x) && gamepad1.x) {
+//            console.display(3, "TrainerMODE; ${goalDetector.displayMode}")
+//            when (goalDetector.displayMode) {
+//                "frame" -> goalDetector.displayMode = "mask"
+//                "mask" -> goalDetector.displayMode = "kernel"
+//                "kernel" -> goalDetector.displayMode = "frame"
+//            }
+//            render()
+//        }
+//
+//
+//        when {
+//            DpadHelper.stateChanged(gamepad1.dpad_left) && gamepad1.dpad_left -> {
+//                when (varBeingEdited) {
+//                    goalDetector.L_H -> varBeingEdited = goalDetector.L_S
+//                    goalDetector.L_S -> varBeingEdited = goalDetector.L_V
+//                    goalDetector.L_V -> varBeingEdited = goalDetector.U_H
+//                    goalDetector.U_H -> varBeingEdited = goalDetector.U_S
+//                    goalDetector.U_S -> varBeingEdited = goalDetector.U_V
+//                    goalDetector.U_V -> varBeingEdited = goalDetector.L_H
+//                }
+//                render()
+//            }
+//        }
+//
+//        if (YbuttonHelper.stateChanged(gamepad1.y) && gamepad1.y) {
+//            console.display(1, "Vals Zeroed")
+//            goalDetector.L_H.value = 0.0
+//            goalDetector.L_S.value = 0.0
+//            goalDetector.L_V.value = 0.0
+//            goalDetector.U_H.value = 0.0
+//            goalDetector.U_S.value = 0.0
+//            goalDetector.U_V.value = 0.0
+//            render()
+//        }
+//
+//        if (RbumperHelper.stateChanged(gamepad1.right_bumper) && gamepad1.right_bumper) {
+//            varBeingEdited.value += 5
+//            render()
+//        }
+//        if (LbumperHelper.stateChanged(gamepad1.left_bumper) && gamepad1.left_bumper) {
+//            varBeingEdited.value -= 5
+//            render()
+//        }
+//
+//        if (AbuttonHelper.stateChanged(gamepad1.a) && gamepad1.a) {
+//            goalDetector.L_H.value = 0.0
+//            goalDetector.L_S.value = 0.0
+//            goalDetector.L_V.value = 0.0
+//            goalDetector.U_H.value = 255.0
+//            goalDetector.U_S.value = 255.0
+//            goalDetector.U_V.value = 255.0
+//            console.display(1, "Vals Squonked")
+//            render()
+//        }
+//    }
+//
+//    override fun loop() {
+//        aimer.updateAimAndAdjustRobot()
+//    }
+//}
+//}
--- TeamCode/src/main/java/creamsicleGoalDetection/CreamsicleMultiPipelineTest.kt
+++ TeamCode/src/main/java/creamsicleGoalDetection/CreamsicleMultiPipelineTest.kt
@@ -1,40 +1,40 @@
-package creamsicleGoalDetection
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import locationTracking.Coordinate
-import openCvAbstraction.OpenCvAbstraction
-import ringDetector.RingDetector
-import robotCode.ChoiVicoHardware
-import robotCode.hardwareClasses.OdometryDriveMovement
-import robotCode.OdometryTestHardware
-import telemetryWizard.TelemetryConsole
-
-@TeleOp(name="Multi Pipeline Test", group="Tests")
-class CreamsicleMultiPipelineTest: LinearOpMode() {
-
-    val console = TelemetryConsole(telemetry)
-    val hardware = ChoiVicoHardware()
-    val robot = OdometryDriveMovement(console, hardware)
-    val opencv = OpenCvAbstraction(this)
-
-    val goalDetector = CreamsicleGoalDetector(console)
-    val turret = UltimateGoalAimer(console, goalDetector, hardware)
-    var position: RingDetector.RingPosition = RingDetector.RingPosition.NONE
-
-
-    val target = Coordinate()
-
-    override fun runOpMode() {
-        hardware.init(hardwareMap)
-
-        opencv.init(hardwareMap)
-        opencv.start()
-
-        waitForStart()
-
-
-        opencv.onNewFrame(goalDetector::scoopFrame)
-        sleep(500000000000)
-    }
-}
+//package creamsicleGoalDetection
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+//import locationTracking.Coordinate
+//import openCvAbstraction.OpenCvAbstraction
+//import ringDetector.RingDetector
+//import robotCode.ChoiVicoHardware
+//import robotCode.hardwareClasses.OdometryDriveMovement
+//import robotCode.OdometryTestHardware
+//import telemetryWizard.TelemetryConsole
+//
+//@TeleOp(name="Multi Pipeline Test", group="Tests")
+//class CreamsicleMultiPipelineTest: LinearOpMode() {
+//
+//    val console = TelemetryConsole(telemetry)
+//    val hardware = ChoiVicoHardware()
+//    val robot = OdometryDriveMovement(console, hardware)
+//    val opencv = OpenCvAbstraction(this)
+//
+//    val goalDetector = CreamsicleGoalDetector(console)
+//    val turret = UltimateGoalAimer(console, goalDetector, hardware)
+//    var position: RingDetector.RingPosition = RingDetector.RingPosition.NONE
+//
+//
+//    val target = Coordinate()
+//
+//    override fun runOpMode() {
+//        hardware.init(hardwareMap)
+//
+//        opencv.init(hardwareMap)
+//        opencv.start()
+//
+//        waitForStart()
+//
+//
+//        opencv.onNewFrame(goalDetector::scoopFrame)
+//        sleep(500000000000)
+//    }
+//}
